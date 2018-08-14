cbfaf4bd0ac64d3463531881376a20b79bab54a1
Beatriz Martins <mariabeatriz.dongamartins@gmail.com>
2018-08-14 03:46:14 +0100

    added categories to the game

--- hangman.py
+++ hangman.py
@@ -11,7 +11,9 @@ def game():
     """Main function that runs the game"""
     print("You're about to start playing hangman, I hope you're ready!")
     name = input("Before we start please tell me your name\n")
-    print("Now that I know your name lets get started. Please remember that your guess must consist of only a letter or the number 1 if you intend to guess the entire word.")
+    print("Now that I know your name lets get started. Please remember that "\
+    "your guess must consist of only a letter or the number 1 if you intend" \
+    "to guess the entire word.")
 
     current_try = 0
     max_tries = 6
@@ -31,7 +33,8 @@ def game():
         if letter_val == '1' or not letter_in_word(final_guess):
             current_try += 1
             tries_left = max_tries - current_try
-            print("Bad luck, wrong guess. You've got " + str(tries_left) + " tries left.")
+            print("Bad luck, wrong guess. You've got " + str(tries_left) + \
+            " tries left.")
             hangman_graphic(current_try)
         else:
             print("That was a great guess!")
@@ -44,21 +47,54 @@ def game():
 
 
 def secret_word_f():
-    """Contains the list of possible words to be used as the secret word and
-    chooses randomly one of them to be the secret word"""
-    list_words = ['humanity', 'television', 'pokemon', 'command', 'mechanical',
-    'parrot', 'surprise', 'family', 'banana', 'character']
-    number_words = len(list_words)
-    word_number = random.randint(0,number_words - 1)
-    return list_words[word_number]
+    """Contains the list of possible categories and then the words associated
+    with each category to be used as the secret word. Chooses randomly one of
+    the categories and then the secret word"""
 
+    #list of words for each category
+    colors = ['red', 'green', 'yellow', 'blue', 'pink', 'purple', 'orange', \
+    'aqua', 'beige', 'gray', 'black', 'brown', 'coral', 'gold', 'salmon', \
+    'violet', 'magenta', 'lime', 'teal']
+    countries = ['croatia', 'singapore', 'hungary', 'ukraine', 'japan', \
+    'netherlands', 'macao', 'canada', 'poland', 'portugal', 'greece', \
+    'thailand', 'austria', 'malasia', 'mexico']
+    animals = ['panda', 'dog', 'cat', 'bison', 'dolphin', 'eagle', 'lobster', \
+    'wolf', 'shark', 'crocodile', 'turtle', 'turkey', 'spider', 'monkey', \
+    'octopus', 'lion', 'bear', 'cow']
+    jobs = ['policeman', 'fireman', 'nurse', 'pilot', 'postman', 'doctor', \
+    'waiter', 'teacher', 'secretary', 'journalist', 'engineer', 'architect', \
+    'actor', 'dentist', 'farmer']
+    food = ['banana', 'orange', 'tomato', 'pasta', 'cheese', 'chocolate', \
+    'pizza', 'sandwich', 'eggs', 'potato', 'cereal', 'yogurt', 'chicken', \
+    'omelet', 'bacon', 'donut', 'burrito']
+    furniture = ['bed', 'wardrobe', 'dresser', 'desk', 'sofa', 'lamp', \
+    'chair', 'armchair', 'bookshelf', 'nightstand', 'mirror', 'sink', \
+    'curtains', 'picture']
+    sports = ['badminton', 'basketball', 'bowling', 'boxing', 'cricket', \
+    'football', 'golf', 'gymnastics', 'handball', 'hockey', 'judo', 'surfing',\
+     'swimming', 'tennis', 'volleyball']
+
+    list_categories = [colors, countries, animals, jobs, food, furniture, \
+    sports]
+    list_string = ['colors', 'countries', 'animals', 'jobs', 'food', \
+    'furniture', 'sports']
+
+    amount_categories = len(list_categories)
+    categories_number = random.randint(0,amount_categories - 1)
+    category_final = list_categories[categories_number]
+    category_string = list_string[categories_number]
+
+    amount_final_category = len(category_final)
+    category_final_number = random.randint(0, amount_final_category - 1)
+    return category_final[category_final_number], category_string
 
 def used_letters(letter):
     """Verifies if the user as already done that guess before and if so allows
     him to do another guess. The new guess is then added to the list of used
     guesses"""
     while letter in list_used:
-        letter = input("You have already choosen this letter, try another one!\n")
+        letter = input("You have already choosen this letter, try another"
+        "one!\n")
     list_used.append(letter)
     return letter
 
@@ -66,13 +102,14 @@ def used_letters(letter):
 def validate_guess():
     """Validates the guess done by the player and allows him to do another
     guess if the one he did was invalid"""
-    letter_inval = input("Insert your guess or the number 1:\n")
+    letter_inval = input("The category is " + str(secret_category) +
+    ". Insert your guess or the number 1:\n")
     if letter_inval == '1':
         return letter_inval
 
-    while len(letter_inval) > 1 or not letter_inval.isalpha(): #or letter_invalid != "try":
-        #esta a ver a primeira codicao e sai logo
-        letter_inval = input("Thats an invalid guess, please insert a new one \n")
+    while len(letter_inval) > 1 or not letter_inval.isalpha():
+        letter_inval = input("Thats an invalid guess, please insert a new one"
+         "\n")
 
     return letter_inval
 
@@ -98,7 +135,8 @@ def current_word(name):
 
 def try_guess(name):
     """Allows player to guess the entire word"""
-    final_word = input("So you think you know the secret word? Go ahead and do your guess: ")
+    final_word = input("So you think you know the secret word? Go ahead and do"
+    "your guess: ")
     if final_word == secret_word:
         print("You won " + name + ", congratulations!")
         exit()
@@ -165,7 +203,7 @@ def hangman_graphic(guess):
 
 #global variables
 list_used = []
-secret_word = secret_word_f()
+secret_word, secret_category = secret_word_f()
 
 #starts the game
 game()
